73271860564ba19362c6b47722227ff0929e20e3
conny <conny@musma.net>
2024-08-16 13:28:33 +0900

    infra setting

---
+++ src/main/java/fastcampus/ad/fastcampusdspmigration/FastcampusDspMigrationApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.ad.fastcampusdspmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusDspMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusDspMigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fastcampus/ad/fastcampusdspmigration/FastcampusDspMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.ad.fastcampusdspmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusDspMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
